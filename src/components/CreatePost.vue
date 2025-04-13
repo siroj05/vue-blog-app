08d934afc677529836a52f465ecaa129350a2de6
siroj05 <rojudin123@gmail.com>
2025-04-13 20:40:41 +0700

    add create post page

--- src/components/CreatePost.vue
+++ src/components/CreatePost.vue
@@ -1,18 +1,48 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+const router = useRouter()
+const title = ref<string>('')
+const desc = ref<string>('')
+const loading = ref(false)
+const onSubmit = async () => {
+  if (title.value.length > 0 && desc.value.length > 0) {
+    loading.value = true
+    try {
+      const res = await fetch('https://dummyjson.com/posts/add', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          title: title.value,
+          body: desc.value,
+          userId: 5,
+        }),
+      })
+      if (!res.ok) throw new Error('failed to fetch')
+      router.push('/')
+    } catch (error) {
+    } finally {
+      loading.value = false
+    }
+  }
+}
+</script>
 <template>
-  <form action="" class="form-layout">
-    <div class="form-input">
-      <label for="title">Title</label>
-      <input id="title" name="title" type="text" />
-    </div>
-    <div class="form-input">
-      <label for="desc">Description</label>
-      <input id="desc" name="desc" type="text" />
-    </div>
-    <div class="position-btn">
-      <button class="post-btn">Post</button>
-    </div>
-  </form>
+  <div>
+    <form action="" class="form-layout" v-on:submit.prevent="onSubmit">
+      <div class="form-input">
+        <label for="title">Title</label>
+        <input id="title" name="title" type="text" v-model="title" required />
+      </div>
+      <div class="form-input">
+        <label for="desc">Description</label>
+        <input id="desc" name="desc" type="text" v-model="desc" required />
+      </div>
+      <div class="position-btn">
+        <button :disabled="loading" class="post-btn">{{ loading ? 'Posting..' : 'Post' }}</button>
+      </div>
+    </form>
+  </div>
 </template>
 <style>
 .form-input {
